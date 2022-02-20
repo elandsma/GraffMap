bca44bf466a2c1e0b7f90fb3ee1b262aef84d7cb
Elias <eplandsman@gmail.com>
2022-02-19 20:38:39 -0500

    fill in location detail page

--- screens/LocationDetailScreen.js
+++ screens/LocationDetailScreen.js
@@ -1,15 +1,74 @@
 import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View, Linking} from 'react-native'
 import {Button, Input, Image} from "react-native-elements";
+import { supabase } from "./../supabase-service";
+import { SUPABASE_URL } from "react-native-dotenv"
 
 const LocationDetailScreen = ( {route, navigation })=>{
     const { lat, long } = route.params;
+    const [artworks, setArtworks] = useState(null);
+    const [dbError, setdbError] = useState(false);
+
+
+    useEffect(()=>{
+        fetchArtwork()
+    }, [])
+
+    /**
+     * Fetch all artwork from given location coords, assign to state
+     */
+     async function fetchArtwork(){
+        try{
+            console.log("start fetchArtwork()")
+            let { data, error } = await supabase
+                .from('artworkdata')
+                .select('*')                
+                .match({lat: lat, long: long})
+                .order('timestamp', {descending: true})
+            if(error){
+                console.log("DB Fetch Error");
+                setdbError(true);
+                throw new Error(error);
+            }
+            setArtworks(data);
+            console.log("data: ", data);
+        }
+        catch (e){
+            console.log("Error: ", e);
+            setdbError(true);
+        }
+    }
 
     console.log("Lat: ", lat);
     console.log("Long: ", long);
+    
+    if(!lat || !long){
+        return(
+            <View style={styles.container}>
+                <Text>Error: No location params</Text>
+            </View>    
+        )
+    }
+
+    if(dbError){
+        return(
+            <View style={styles.container}>
+                <Text>Error accessing database.</Text>
+            </View>
+        )
+    }
+
     return(
         <View style={styles.container}>
-            <Text>{lat}, {long}</Text>
+            {artworks?
+            <>
+                    <Text>{lat}, {long}</Text>
+            </>
+            :
+            <>
+                <Text>Fetching Artwork...</Text>
+            </>
+        }
         </View>
     )
 }
